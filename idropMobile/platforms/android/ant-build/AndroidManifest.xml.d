e65a7eeebface20155a2d08517ba8432aa65272e
mconway <michael.c.conway@gmail.com>
2014-11-08 09:15:40 -0500

    #1 more ant build

--- idropMobile/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/AndroidManifest.xml \
--- idropMobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/ant-build/classes.dex \
-/Users/mikeconway/Documents/workspace-mobile/idrop-mobile/idropMobile/platforms/android/CordovaLib/ant-build/classes.jar \
